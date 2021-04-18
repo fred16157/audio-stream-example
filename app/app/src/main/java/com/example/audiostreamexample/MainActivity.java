87abf098a2e285b7b01b5914a7e916987b7e6c6e
강현구 <fred16157@gmail.com>
2021-04-18 14:43:30 +0900

    재생 시간 업데이트 구현

--- app/app/src/main/java/com/example/audiostreamexample/MainActivity.java
+++ app/app/src/main/java/com/example/audiostreamexample/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -20,17 +21,18 @@ import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
-    TextView currentTextView;
-    TextView durationTextView;
+    TextView currentTextView, durationTextView;
     ImageButton togglePlayBtn;
     EditText serverUrlEditText;
     Button connectBtn;
-    MediaPlayer mediaPlayer;
+    static MediaPlayer mediaPlayer;
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
         togglePlayBtn = findViewById(R.id.togglePlayBtn);
         serverUrlEditText = findViewById(R.id.serverUrlEditText);
         connectBtn = findViewById(R.id.connectBtn);
+        timer = new Timer();
+        //초마다 현재 시간과 프로그레스 바를 업데이트 하기 위해 실행
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(mediaPlayer == null) return;
+                if(mediaPlayer.isPlaying()) {
+                    runOnUiThread(() -> {
+                        progressBar.setProgress(mediaPlayer.getCurrentPosition());
+                        currentTextView.setText(formatMillis(mediaPlayer.getCurrentPosition()));
+                    });
+                }
+            }
+        }, 0, 1000);
+
+        //재생 토글
         togglePlayBtn.setOnClickListener(v -> {
             if(mediaPlayer.isPlaying()) {
                 togglePlayBtn.setImageDrawable(ResourcesCompat.getDrawable(getResources(), android.R.drawable.ic_media_play, null));
@@ -53,21 +71,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //연결 버튼 클릭
         connectBtn.setOnClickListener(v -> {
             try {
                 mediaPlayer = new MediaPlayer();
+                //재생 준비 완료
                 mediaPlayer.setOnPreparedListener(mp -> {
+                    durationTextView.setText(formatMillis(mp.getDuration()));
+                    progressBar.setMax(mp.getDuration());
                     mp.start();
                     togglePlayBtn.setImageDrawable(ResourcesCompat.getDrawable(getResources(), android.R.drawable.ic_media_pause, null));
                 });
-
+                //재생 끝
+                mediaPlayer.setOnCompletionListener(mp -> {
+                    togglePlayBtn.setImageDrawable(ResourcesCompat.getDrawable(getResources(), android.R.drawable.ic_media_play, null));
+                });
+                //에러 발생
                 mediaPlayer.setOnErrorListener((mp, what, extra) -> {
                     Snackbar.make(findViewById(R.id.mainLayout), "재생에 실패했습니다.", Snackbar.LENGTH_LONG);
                     return false;
                 });
 
                 mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                //서버의 URL로 설정하면 HTTP로 파일이 불러와짐
                 mediaPlayer.setDataSource(serverUrlEditText.getText().toString());
+                //비동기 준비, 준비 완료시 콜백 실행
                 mediaPlayer.prepareAsync();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -82,4 +110,10 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.release();
         mediaPlayer = null;
     }
+
+    public String formatMillis(long millis) {
+        long s = millis / 1000;
+        return String.format("%02d:%02d", s / 60, (s % 60));
+    }
+
 }
